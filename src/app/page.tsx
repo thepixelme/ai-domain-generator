c5d6bd81fb36a12163778b9905101df516431a12
Nhat Nguyen <nhat@thepixelme.com>
2025-05-29 01:02:25 -0400

    basic site header structure

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,12 @@
 import Image from "next/image";
 import { Button } from "@heroui/button";
 
+import { SiteHeader } from "@/components/header";
+
 export default function Home() {
   return (
     <div>
+      <SiteHeader />
       <Button color="primary">Button</Button>
     </div>
   );
---
+++ src/components/header/header.tsx
@@ -0,0 +1,43 @@
+'use client';
+
+import { Navbar, NavbarBrand, NavbarContent, NavbarItem, Link, Button } from "@heroui/react";
+import { use } from "react";
+
+export const Header = () => {
+  return (
+    <Navbar position="static">
+      <NavbarBrand>
+        <Link href="/" className="text-xl font-bold">
+          Domain Generator
+        </Link>
+      </NavbarBrand>
+      <NavbarContent className="hidden sm:flex gap-4" justify="center">
+        <NavbarItem>
+          <Link color="foreground" href="#">
+            Features
+          </Link>
+        </NavbarItem>
+        <NavbarItem isActive>
+          <Link aria-current="page" href="#">
+            Customers
+          </Link>
+        </NavbarItem>
+        <NavbarItem>
+          <Link color="foreground" href="#">
+            Integrations
+          </Link>
+        </NavbarItem>
+      </NavbarContent>
+      <NavbarContent justify="end">
+        <NavbarItem className="hidden lg:flex">
+          <Link href="#">Login</Link>
+        </NavbarItem>
+        <NavbarItem>
+          <Button as={Link} color="primary" href="#" variant="flat">
+            Sign Up
+          </Button>
+        </NavbarItem>
+      </NavbarContent>
+    </Navbar>
+  );
+}
